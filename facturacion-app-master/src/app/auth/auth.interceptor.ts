f730f5d324d750420839fdaa98e262233dd990fa
Luis Romero <luisdrh34@gmail.com>
2026-01-26 17:46:52 -0500

    Fix dashboard shortcuts, table styles, and merge conflict

--- facturacion-app-master/src/app/auth/auth.interceptor.ts
+++ facturacion-app-master/src/app/auth/auth.interceptor.ts
@@ -1,21 +1,4 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-<<<<<<< HEAD
-
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const token = localStorage.getItem('token');
-    console.log('AuthInterceptor: Token found?', !!token); // DEBUG
-
-    if (token) {
-        const cloned = req.clone({
-            setHeaders: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        return next(cloned);
-    }
-
-    return next(req);
-=======
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
 
@@ -33,5 +16,4 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   }
 
   return next(req);
->>>>>>> d4cd851e994a52d64f272faed8d0e4ab9759d718
 };
