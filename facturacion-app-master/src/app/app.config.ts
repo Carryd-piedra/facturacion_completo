76582ae1d7446c418646e41a2334d8677d02b7ea
Luis Romero <luisdrh34@gmail.com>
2026-01-27 07:34:56 -0500

    Actualizar configuración regional a español, ajustes de UI en productos y credenciales de base de datos

--- facturacion-app-master/src/app/app.config.ts
+++ facturacion-app-master/src/app/app.config.ts
@@ -1,5 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
 
 import { routes } from './app.routes';
 
@@ -10,6 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LOCALE_ID, useValue: 'es' }
   ]
 };
